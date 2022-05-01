6144d088d01ee95dca0f3b32688e90838161a1de
avalmercafe02@gmail.com <allenkim@Allens-MacBook-Pro.local>
2022-05-01 13:06:01 -0400

    commit tree 94d1d0dcefbdd538dd15febfe78fe1f990628630

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b) {
+return a+b
+}
+console.log(add(500,578))   
+
+function subtract(a,b) {
+    return a-b
+}
+console.log (subtract(1,2))
+
+function multiply(a,b) {
+    return a*b
+
+}
+console.log(multiply(43,79))
+
+function divide(a,b) {
+    return a/b
+
+}
+console.log (divide(0.71505958829, 5))
+
+function increment(a){
+    return ++a
+}
+console.log(increment(755))
+
+function decrement(a){
+    return --a
+}
+console.log(decrement())
+
+function makeInt(a) {
+    return parseInt(a,10)
+}
+console.log(makeInt())
+
+function preserveDecimal(a) {
+    return parseFloat(a)
+}
+console.log(parseFloat())
